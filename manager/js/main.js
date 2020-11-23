b269c35e5fd1c7f0404286d1c2f37f1b040f301c
Dmitry Naidich <dmitry@naidich.ru>
2020-11-23 13:50:24 +0300

    Дополнительная проверка токена

--- manager/js/main.js
+++ manager/js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
 	
+	// Подгрузка страниц
 	$(".tree a").click(function(e){
 		e.preventDefault();
+		
+		/* Дополнительная проверка токена перед подгрузкой страниц
+		* Особенно важно, когда на странице совершаются действия, для которых нужен валидный токен */
+		checkToken();
 		
 		$("#main-view").load("/manager/" + $(this).attr("href"));
 	});
