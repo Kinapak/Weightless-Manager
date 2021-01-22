28b887e29b8d27e5975bb167d575d60dbdd25a47
Dmitry Naidich <dmitry@naidich.ru>
2021-01-22 17:24:28 +0300

    Обработка ошибки неправильных идентификационных данных при входе

--- login/js/login.js
+++ login/js/login.js
@@ -10,7 +10,12 @@
 	$("#login-form").on("submit", function(e){
 		e.preventDefault();
 		
-		$(this).find("[type='submit']").html("<i class='fa fa-spinner fa-pulse fa-2x'></i>").attr("type", "button");
+		// Экземпляр кнопки входа
+		let $btn = $(this).find("[type='submit']");
+		$btn.html("<i class='fa fa-spinner fa-pulse fa-2x'></i>").attr("type", "button");
+		
+		// Удаление сообщения об ошибке, если есть
+		$(".login100-form-btn").parent().find("p").remove();
 		
 		$.ajax({
 			url: config.api_user_login + "/token",
@@ -29,9 +34,20 @@
 					localStorage.setItem("user_token", user.access_token);
 					localStorage.setItem("IAM_token", IAM.access_token);
 					location.href = "/";
+				} else{ // Вывод ошибки и выделение полей
+					$("input").addClass("error-field").parent().addClass('alert-validate');
+					$(".login100-form-btn").parent().prepend("<p>Некорректно указан логин или пароль</p>");
+					$btn.html("Войти").attr("type", "submit");
 				}
 			}
 		});
+	});
+	
+	// Удаление сообщений об ошибке
+	$("input").focusin(function(){
+		$(".wrap-input100").removeClass("alert-validate");
+		$("input").removeClass("error-field");
+		$(".login100-form-btn").parent().find("p").remove();
 	});
 	
 })(jQuery);
