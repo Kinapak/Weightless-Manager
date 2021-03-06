96433cb683ab9a215a76633e972c72f863bbec9b
Dmitry Naidich <dmitry@naidich.ru>
2021-03-06 17:47:10 +0300

    CDN DataTable Russian

--- manager/js/dbAction.js
+++ manager/js/dbAction.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
 						data: result.tables.empty || result.tables,
 						paging: false,
 						language: {
-							url: "manager/js/plugins/dataTables.russian.json"
+							url: "https://russiabase.ru/wm/v0.2.1/manager/js/plugins/dataTables.russian.json"
 						}
 					});
 				
@@ -144,7 +144,7 @@ $(document).ready(function(){
 					columns: result.cols,
 					pageLength: 50,
 					language: {
-						url: "manager/js/plugins/dataTables.russian.json"
+						url: "https://russiabase.ru/wm/v0.2.1/manager/js/plugins/dataTables.russian.json"
 					}
 				});
 				
